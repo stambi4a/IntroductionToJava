83c10b5c6ca96de0f6da7d9997a50d1c5a4fb16b
stambi4a <cityzen_s@abv.bg>
2016-03-24 22:14:23 +0200

    SaAS

--- PrintCharacters.java
+++ PrintCharacters.java
@@ -3,7 +3,7 @@ public class PrintCharacters {
     public static void main(String[] args) {
         for(char ch = 'a'; ch <= 'z'; ch++)
         {
-            System.out.print(ch + " ");
+            System.out.print(ch + "(+-) ");
         }
     }
 }
